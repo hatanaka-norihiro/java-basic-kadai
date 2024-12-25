1f887f0a60456e36e85a6fc4dea0586606c86b42
hatanaka-norihiro <hatanaka@alto.ocn.ne.jp>
2024-12-25 21:43:23 +0900

    KatoExec_Chapter18.javaの再修正

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -6,17 +6,17 @@ public class KatoExec_Chapter18 {
 
 		//加藤太郎
 		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
-		KatoTaro.setGivenName("太郎");
+		KatoTaro.setGivenName();
 		KatoTaro.execIntroduce();
 		
 		//加藤一郎
 		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
-		KatoIchiro.setGivenName("一郎");
+		KatoIchiro.setGivenName();
 		KatoIchiro.execIntroduce();
 		
 		//加藤花子
 		KatoHanako_Chapter18 KatoHanako = new KatoHanako_Chapter18();
-		KatoHanako.setGivenName("花子");
+		KatoHanako.setGivenName();
 		KatoHanako.execIntroduce();
 	}
 
