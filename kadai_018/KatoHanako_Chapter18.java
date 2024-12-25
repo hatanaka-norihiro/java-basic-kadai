665946e3987c2db0a1a2257e320fa8b1f1baede7
hatanaka-norihiro <hatanaka@alto.ocn.ne.jp>
2024-12-25 21:44:31 +0900

    KatoHanako_Chapter18.javaの再修正

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -6,10 +6,10 @@ public class KatoHanako_Chapter18 extends Kato_Chapter18 {
 	public KatoHanako_Chapter18(){
 	}
 	//名を表すフィールドの値をセットする
-    public void setGivenName(String value) {
-        givenName = value;
-    }
-    //自己紹介
+    	public void setGivenName() {
+        	givenName = "花子";
+    	}
+    	//自己紹介
 	public void eachIntroduce(){
 		System.out.println("趣味は読書です");
 	}
