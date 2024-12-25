de0f072396f27cf99f2ecf8f41eea41b06ae967c
hatanaka-norihiro <hatanaka@alto.ocn.ne.jp>
2024-12-25 18:04:40 +0900

    KatoTaro_Chapter18.javaの修正

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -4,14 +4,11 @@ public class KatoTaro_Chapter18 extends Kato_Chapter18 {
 
 	//インストラクタ(初期化)
 	public KatoTaro_Chapter18(){
-		familyName = "加藤";
-		address = "東京都中野区〇×";
-		setGivenName("太郎");
 	}
 	//名を表すフィールドの値をセットする
-    public void setGivenName(String value) {
-        givenName = value;
-    }
+    	public void setGivenName(String value) {
+        	givenName = value;
+    	}
 	//自己紹介
 	public void eachIntroduce(){
 		System.out.println("Javaが得意です");
